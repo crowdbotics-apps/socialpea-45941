b45babca5d65c6c4f61a9780bd6197676cae632f
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:32:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "socialpea-45941",
+  url: "https://socialpea-45941.botics.co",
+  api: "https://socialpea-45941.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
